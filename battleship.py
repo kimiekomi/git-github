e5e0c0e0284cc032ffaffafd76859fe0c2d96515
Shelby Kim <93174976+kimiekomi@users.noreply.github.com>
2022-03-07 21:11:12 +0000

    created game and board class...moved appropriate functions inside them

--- battleship.py
+++ battleship.py
@@ -3,8 +3,158 @@
 from random import randint
 import os
 
-#Ship Class
-class Ship:
+
+class Game():
+    os.system('clear')
+    print_board(board_display)
+
+    for turn in range(num_turns):
+        print("Turn:", turn + 1, "of", num_turns)
+        print("Ships left:", len(ship_list))
+        print()
+    
+        guess_coords = {}
+        
+        while True:
+            guess_coords['row'] = get_row()
+            guess_coords['col'] = get_col()
+        
+            if board_display[guess_coords['row']][guess_coords['col']] == 'X' or \
+        board_display[guess_coords['row']][guess_coords['col']] == '*':
+                print("\nYou guessed that one already.")
+        
+            else:
+                break
+
+        os.system('clear')
+
+        ship_hit = False
+    
+        for ship in ship_list:
+            if ship.contains(guess_coords):
+                print("Hit!")
+                ship_hit = True
+                board_display[guess_coords['row']][guess_coords['col']] = 'X'
+        
+                if ship.destroyed():
+                    print("Ship Destroyed!")
+                    ship_list.remove(ship)
+        
+                break
+                
+        if not ship_hit:
+            board_display[guess_coords['row']][guess_coords['col']] = '*'
+            print("You missed!")
+
+        print_board(board_display)
+    
+        if not ship_list:
+            break
+
+    # End Game
+    if ship_list:
+        print("You lose!")
+    else:
+        print("All the ships are sunk. You win!")
+
+
+    def search_locations(self, size, orientation):
+        locations = []
+    
+        if orientation != 'horizontal' and orientation != 'vertical':
+            raise ValueError("Orientation must have a value of either 'horizontal' or 'vertical'.")
+    
+        if orientation == 'horizontal':
+            if size <= col_size:
+                for r in range(row_size):
+                    for c in range(col_size - size + 1):
+                        if 1 not in board[r][c:c+size]:
+                            locations.append({'row': r, 'col': c})
+                  
+        elif orientation == 'vertical':
+            if size <= row_size:
+                for c in range(col_size):
+                    for r in range(row_size - size + 1):
+                        if 1 not in [board[i][c] for i in range(r, r+size)]:
+                            locations.append({'row': r, 'col': c})
+    
+        if not locations:
+            return 'None'
+      
+        return locations
+
+    def random_location(self):
+        size = randint(min_ship_size, max_ship_size)
+        orientation = 'horizontal' if randint(0, 1) == 0 else 'vertical'
+    
+        locations = search_locations(size, orientation)
+        
+        if locations == 'None':
+            return 'None'
+      
+        return {'location': locations[randint(0, len(locations) - 1)], 'size': size,\
+         'orientation': orientation}
+
+
+    def get_row(self):
+        while True:
+            try:
+                guess = int(input("Row Guess: "))
+                
+                if guess in range(1, row_size + 1):
+                    return guess - 1
+          
+                print("\nOops, that's not even in the ocean.")
+                    
+            except ValueError:
+                print("\nPlease enter a number")
+
+
+    def get_col(self):
+        while True:
+            try:
+                guess = int(input("Column Guess: "))
+          
+                if guess in range(1, col_size + 1):
+                    return guess - 1
+          
+                print("\nOops, that's not even in the ocean.")
+        
+            except ValueError:
+                print("\nPlease enter a number")
+
+
+    def contains(self, location):
+        for coords in self.coordinates:
+            
+            if coords == location:
+                return True
+            
+        return False
+
+
+    def destroyed(self):
+        for coords in self.coordinates:
+            
+            if board_display[coords['row']][coords['col']] == 'O':
+                return False
+            
+            elif board_display[coords['row']][coords['col']] == '*':
+                raise RuntimeError("Board display inaccurate")
+        
+        return True
+
+
+    def print_board(self, board_array):
+        print("\n  " + " ".join(str(x) for x in range(1, col_size + 1)))
+        
+        for r in range(row_size):
+            print(str(r + 1) + " " + " ".join(str(c) for c in board_array[r]))
+            
+        print()
+
+
+class Board:
     def __init__(self, size, orientation, location):
         self.size = size
         
@@ -80,167 +230,17 @@ class Ship:
             board[coords['row']][coords['col']] = 1
 
 
-    def contains(self, location):
-        for coords in self.coordinates:
-            
-            if coords == location:
-                return True
-            
-        return False
-
-
-    def destroyed(self):
-        for coords in self.coordinates:
-            
-            if board_display[coords['row']][coords['col']] == 'O':
-                return False
-            
-            elif board_display[coords['row']][coords['col']] == '*':
-                raise RuntimeError("Board display inaccurate")
-        
-        return True
-
-        # Create the ships
-
-        temp = 0
-        while temp < self.num_ships:
-            ship_info = random_location()
-
-            if ship_info == 'None':
-                continue
-        
-            ship_list.append(Ship(ship_info['size'], ship_info['orientation'], ship_info['location']))
-            temp += 1
-            
-        del temp
-
-        # Play Game
-        os.system('clear')
-        print_board(board_display)
-
-        for turn in range(num_turns):
-            print("Turn:", turn + 1, "of", num_turns)
-            print("Ships left:", len(ship_list))
-            print()
-        
-            guess_coords = {}
-            
-            while True:
-                guess_coords['row'] = get_row()
-                guess_coords['col'] = get_col()
-            
-                if board_display[guess_coords['row']][guess_coords['col']] == 'X' or \
-            board_display[guess_coords['row']][guess_coords['col']] == '*':
-                    print("\nYou guessed that one already.")
-            
-                else:
-                    break
-
-            os.system('clear')
-
-            ship_hit = False
-        
-            for ship in ship_list:
-                if ship.contains(guess_coords):
-                    print("Hit!")
-                    ship_hit = True
-                    board_display[guess_coords['row']][guess_coords['col']] = 'X'
-            
-                    if ship.destroyed():
-                        print("Ship Destroyed!")
-                        ship_list.remove(ship)
-            
-                    break
-                    
-            if not ship_hit:
-                board_display[guess_coords['row']][guess_coords['col']] = '*'
-                print("You missed!")
-
-            print_board(board_display)
-        
-            if not ship_list:
-                break
-
-        # End Game
-        if ship_list:
-            print("You lose!")
-        else:
-            print("All the ships are sunk. You win!")
-
-
-
-#Functions
-def print_board(board_array):
-    print("\n  " + " ".join(str(x) for x in range(1, col_size + 1)))
     
-    for r in range(row_size):
-        print(str(r + 1) + " " + " ".join(str(c) for c in board_array[r]))
-        
-    print()
 
 
-def search_locations(size, orientation):
-    locations = []
+class Ship:
+    temp = 0
+    while temp < self.num_ships:
+        ship_info = random_location()
 
-    if orientation != 'horizontal' and orientation != 'vertical':
-        raise ValueError("Orientation must have a value of either 'horizontal' or 'vertical'.")
-
-    if orientation == 'horizontal':
-        if size <= col_size:
-            for r in range(row_size):
-                for c in range(col_size - size + 1):
-                    if 1 not in board[r][c:c+size]:
-                        locations.append({'row': r, 'col': c})
-              
-    elif orientation == 'vertical':
-        if size <= row_size:
-            for c in range(col_size):
-                for r in range(row_size - size + 1):
-                    if 1 not in [board[i][c] for i in range(r, r+size)]:
-                        locations.append({'row': r, 'col': c})
-
-    if not locations:
-        return 'None'
-  
-    return locations
-
-def random_location():
-    size = randint(min_ship_size, max_ship_size)
-    orientation = 'horizontal' if randint(0, 1) == 0 else 'vertical'
-
-    locations = search_locations(size, orientation)
+        if ship_info == 'None':
+            continue
     
-    if locations == 'None':
-        return 'None'
-  
-    return {'location': locations[randint(0, len(locations) - 1)], 'size': size,\
-     'orientation': orientation}
-
-
-def get_row():
-    while True:
-        try:
-            guess = int(input("Row Guess: "))
-            
-            if guess in range(1, row_size + 1):
-                return guess - 1
-      
-            print("\nOops, that's not even in the ocean.")
-                
-        except ValueError:
-            print("\nPlease enter a number")
-
-
-def get_col():
-    while True:
-        try:
-            guess = int(input("Column Guess: "))
-      
-            if guess in range(1, col_size + 1):
-                return guess - 1
-      
-            print("\nOops, that's not even in the ocean.")
-    
-        except ValueError:
-            print("\nPlease enter a number")
+        ship_list.append(Ship(ship_info['size'], ship_info['orientation'], ship_info['location']))
+        temp += 1
 
